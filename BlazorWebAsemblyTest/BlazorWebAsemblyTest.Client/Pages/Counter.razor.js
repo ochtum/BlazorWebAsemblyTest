3a21ef8e6685a524b6d27da574799e44f0e88414
ochtum <okutoumu@gmail.com>
2025-01-23 13:47:04 +0900

    新規作成

---
+++ BlazorWebAsemblyTest/BlazorWebAsemblyTest.Client/Pages/Counter.razor.js
@@ -0,0 +1,13 @@
+﻿export function returnArrayAsync() {
+    // WebAsemblyのメソッド呼出
+    DotNet.invokeMethodAsync('BlazorWebAsemblyTest.Client', 'ReturnArrayAsync')
+        .then(data => {
+            console.log(data);
+        });
+}
+
+// razorコンポーネントから呼び出されるメソッド
+export function addHandlers() {
+    const btn = document.getElementById("btn");
+    btn.addEventListener("click", returnArrayAsync);
+}
